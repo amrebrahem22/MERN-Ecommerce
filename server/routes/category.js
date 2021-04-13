7510e7ddb4dffca53cd314f338523b2bb5a168ba
amrebrahem226 <amrebrahem226@gmail.com>
2021-04-13 15:33:58 +0200

    Category Route

---
+++ server/routes/category.js
@@ -0,0 +1,15 @@
+const  express = require('express');
+const router = express.Router();
+
+// Middleware
+const { authCheck, adminCheck } = require('../middlewares/auth');
+
+const { create, update, read, remove, list } = require('../controllers/category.js');
+
+router.post('/category', authCheck, adminCheck, create);
+router.get('/categories', list);
+router.get('/category/:slug', authCheck, adminCheck, read);
+router.put('/category/:slug', authCheck, adminCheck, update);
+router.delete('/category/:slug', authCheck, adminCheck, remove);
+
+module.exports = router;
