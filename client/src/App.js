38d519837d87891515c44ba05cd3979382112f11
amrebrahem226 <amrebrahem226@gmail.com>
2021-05-15 10:17:53 +0200

    Code spilitting with react lazy and suspense

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { auth } from './firebase';
 import { useDispatch } from 'react-redux';
-import './App.css';
-import Home from './pages/Home';
-import Product from './pages/Product';
-import Category from './pages/Category';
-import Shop from './pages/Shop';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import Sub from './pages/Sub';
-import Payment from './pages/Payment';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
 import SideDrawer from './components/drawer/SideDrawer';
-import RegisterComplete from './pages/auth/RegisterComplete';
-import ForgotPassword from './pages/auth/ForgotPassword';
-import AdminDashboard from './pages/admin/AdminDashboard';
-import CategoryCreate from './pages/admin/category/CategoryCreate';
-import CategoryUpdate from './pages/admin/category/CategoryUpdate';
-import ProductCreate from './pages/admin/product/ProductCreate';
-import ProductUpdate from './pages/admin/product/ProductUpdate';
-import AllProducts from './pages/admin/product/AllProducts';
-import SubCreate from './pages/admin/sub/SubCreate';
-import SubUpdate from './pages/admin/sub/SubUpdate';
-import CreateCoupon from './pages/admin/coupon/CreateCoupon';
-import History from './pages/user/History';
-import Wishlist from './pages/user/Wishlist';
-import Password from './pages/user/Password';
 import Header from './components/nav/Header';
 import { currentUser } from './functions/auth';
 import UserRouter from './components/routes/UserRouter';
 import AdminRouter from './components/routes/AdminRouter';
+import {LoadingOutlined} from '@ant-design/icons';
+import './App.css';
+// Lazy
+const Home = lazy(() => import('./pages/Home'));
+const Product = lazy(() => import('./pages/Product'));
+const Category = lazy(() => import('./pages/Category'));
+const Shop = lazy(() => import('./pages/Shop'));
+const Cart = lazy(() => import('./pages/Cart'));
+const Checkout = lazy(() => import('./pages/Checkout'));
+const Sub = lazy(() => import('./pages/Sub'));
+const Payment = lazy(() => import('./pages/Payment'));
+const Login = lazy(() => import('./pages/auth/Login'));
+const Register = lazy(() => import('./pages/auth/Register'));
+const RegisterComplete = lazy(() => import('./pages/auth/RegisterComplete'));
+const ForgotPassword = lazy(() => import('./pages/auth/ForgotPassword'));
+const AdminDashboard = lazy(() => import('./pages/admin/AdminDashboard'));
+const CategoryCreate = lazy(() => import('./pages/admin/category/CategoryCreate'));
+const CategoryUpdate = lazy(() => import('./pages/admin/category/CategoryUpdate'));
+const ProductCreate = lazy(() => import('./pages/admin/product/ProductCreate'));
+const ProductUpdate = lazy(() => import('./pages/admin/product/ProductUpdate'));
+const AllProducts = lazy(() => import('./pages/admin/product/AllProducts'));
+const SubCreate = lazy(() => import('./pages/admin/sub/SubCreate'));
+const SubUpdate = lazy(() => import('./pages/admin/sub/SubUpdate'));
+const CreateCoupon = lazy(() => import('./pages/admin/coupon/CreateCoupon'));
+const History = lazy(() => import('./pages/user/History'));
+const Wishlist = lazy(() => import('./pages/user/Wishlist'));
+const Password = lazy(() => import('./pages/user/Password'));
 
 function App() {
   const dispatch = useDispatch();
@@ -66,7 +68,15 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <Suspense
+      fallback={
+        <div className="col text-center p-5">
+          __ React Redux EC
+          <LoadingOutlined />
+          MMERCE __
+        </div>
+      }
+      >
       <Header />
       <SideDrawer />
       <ToastContainer />
@@ -96,7 +106,7 @@ function App() {
         <Route exact path='/checkout' component={Checkout} />
         <Route exact path='/payment' component={Payment} />
       </Switch>
-    </div>
+    </Suspense>
   );
 }
 
